4671225c2f2fc9895ce4825f9c606ada1f806c11
Tyler Baldwin <tyler.baldwin.17@cnu.edu>
2020-03-19 15:07:09 -0400

    save

--- app/src/main/java/com/example/proj3_475/MainActivity.java
+++ app/src/main/java/com/example/proj3_475/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.proj3_475;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,13 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.);
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent
+        }
         return true;
     }
 }
