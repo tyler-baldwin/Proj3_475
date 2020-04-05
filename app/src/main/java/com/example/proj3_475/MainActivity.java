01d441f4e9527323e3598893357c5c6fb6d5cbc5
Tyler Baldwin <tyler.baldwin.17@cnu.edu>
2020-04-05 16:15:40 -0400

    hello?

--- app/src/main/java/com/example/proj3_475/MainActivity.java
+++ app/src/main/java/com/example/proj3_475/MainActivity.java
@@ -3,25 +3,37 @@ package com.example.proj3_475;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
-public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     ConnectivityCheck myCheck;
     String info_choice;
+    List<pet> pets;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        setupSimpleSpinner();
+
 
         SharedPreferences a = PreferenceManager.getDefaultSharedPreferences(this);
         a.registerOnSharedPreferenceChangeListener(this);
@@ -31,11 +43,40 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 
         doNetworkCheck(findViewById(android.R.id.content).getRootView());
         doWirelessCheck(findViewById(android.R.id.content).getRootView());
-
-
-
-
     }
+
+    Spinner spinner;
+
+    private void setupSimpleSpinner() {
+        //create a data adapter to fill above spinner with choices
+        //R.array.numbers is arraylist in strings.xml
+        //R.layout.spinner_item_simple is just a textview
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.info_choice, R.layout.spinner_item_simple);
+
+        //get a reference to the spinner
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        //bind the spinner to the datasource managed by adapter
+        if (spinner != null)
+            spinner.setAdapter(adapter);
+        //respond when spinner clicked
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public static final int SELECTED_ITEM = 0;
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int pos, long rowid) {
+                if (arg0.getChildAt(SELECTED_ITEM) != null) {
+                    ((TextView) arg0.getChildAt(SELECTED_ITEM)).setTextColor(Color.WHITE);
+                    Toast.makeText(MainActivity.this, (String) arg0.getItemAtPosition(pos), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
+    }
+
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         getPrefValues(PreferenceManager.getDefaultSharedPreferences((Context) this));
@@ -68,7 +109,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         // TODO Auto-generated method stub
         super.onResume();
         Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
-        SharedPreferences a=PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences a = PreferenceManager.getDefaultSharedPreferences(this);
         info_choice = a.getString("website", "CNU - Defender");
 
     }
@@ -87,4 +128,9 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     }
 
 
+}
+
+class pet {
+    public String name;
+    public String url;
 }
